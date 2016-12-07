3788fbefacfb64eb19e22e86392b8e7a89cedad8
Dari <dari.k@abv.bg>
2016-12-07 13:12:34 +0200

    header version 1 completed

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+// Home page rotating words in key message
+var $rotatingWords = $('.key-messages-content-rotating-words');
+var rotatingWordsContent = ['modern', 'responsive', 'super fast'];
+var wordIndex = 1;
+
+//Changes words in key message on home page
+function changeWord() {
+	$rotatingWords.fadeOut(200, function(){
+		$rotatingWords.text(rotatingWordsContent[wordIndex]).fadeIn(200);	
+	});
+	if ( wordIndex < 2) {
+		wordIndex += 1;
+	} else {
+		wordIndex = 0;
+	}
+}
+
+//Call function on regular intervals to change word in key message on home page
+setInterval(function(){ 
+	changeWord();
+}, 2000);
